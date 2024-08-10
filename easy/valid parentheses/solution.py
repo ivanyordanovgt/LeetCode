678e515ea158f2e4ca2f303c76a8d6decd6882d4
Ivan <vankata294n4@gmail.com>
2024-08-10 13:13:27 +0300

    Covered edge case in which only opened parentheses are given

--- easy/valid parentheses/solution.py
+++ easy/valid parentheses/solution.py
@@ -9,13 +9,14 @@ def solution(s):
     }
 
     opened_stack = deque()
-
+    to_close = 0
     if len(s) % 2 != 0:
         return False
 
     for char in s:
         if char in list(parentheses.keys()):
             opened_stack.append(char)
+            to_close += 1
 
         else:
             if len(opened_stack) == 0:
@@ -24,9 +25,14 @@ def solution(s):
             if char != parentheses[opened_stack.pop()]:
                 return False
 
+            to_close -= 1
+
+    if to_close != 0:
+        return False
+
     return True
 
 
-input = "(("
+input = "()"
 result = solution(input)
 print(result)
