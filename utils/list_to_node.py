a2db4cc969dea926b0d94076272c86ff7e32ae87
Ivan <vankata294n4@gmail.com>
2024-08-11 19:06:15 +0300

    Added utils for input saving

---
+++ utils/list_to_node.py
@@ -0,0 +1,26 @@
+from collections import deque
+from tree_node import TreeNode
+
+
+def listToTreeNode(lst):
+    if not lst:  # If the list is empty, return None
+        return None
+
+    root = TreeNode(lst[0])
+    queue = deque([root])
+    i = 1
+
+    while i < len(lst):
+        current = queue.popleft()
+
+        if lst[i] is not None:
+            current.left = TreeNode(lst[i])
+            queue.append(current.left)
+        i += 1
+
+        if i < len(lst) and lst[i] is not None:
+            current.right = TreeNode(lst[i])
+            queue.append(current.right)
+        i += 1
+
+    return root
